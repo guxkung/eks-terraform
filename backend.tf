41515a906374fe266af6a9b35b92aa48437d1825
V-ris Jaijongrak <v.jaijongrak@gmail.com>
2025-01-10 18:55:17 +0700

    update AWS provider to ~> 5.83.0 to enable ap-southeast-7 support

--- backend.tf
+++ backend.tf
@@ -1,8 +1,13 @@
 terraform {
+  backend "s3" {
+    bucket       = "<YOUR-BUCKET-HERE>"
+    key          = "<OBJECT-KEY>"
+    use_lockfile = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.83.0"
     }
   }
 }
